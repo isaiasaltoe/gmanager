adda344ed95dbb2c8c216b1f1698871036d10e43
isaiasaltoe <isaiasaltoe7@gmail.com>
2026-05-20 16:42:23 -0300

    feat: add lobby action and view with redirect logic for user authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,26 @@
 class PagesController < ApplicationController
+
+  before_action :redirect_if_logged_in, only: [:home]
+  before_action :redirect_if_logged_out, only: [:lobby]
+
   def home
+
+  end
+
+  def lobby
+  end
+
+  private
+
+  def redirect_if_logged_in
+    if user_signed_in?
+      redirect_to lobby_path
+    end
+  end
+
+  def redirect_if_logged_out
+    if !user_signed_in?
+      redirect_to root_path 
+    end
   end
 end
