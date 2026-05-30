77b976ba4cb24e9243130cb1c4189cb4e8b77708
isaiasaltoe <isaiasaltoe7@gmail.com>
2026-05-29 21:17:50 -0300

    feat: enhance styling and layout; add lobby view with team creation links; update fixtures for teams and users

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class TeamTest < ActiveSupport::TestCase
+
+  test "team name cant be blank" do
+    team = Team.new()
+
+    assert_not team.valid?
+  end
   # test "the truth" do
   #   assert true
   # end
-end
